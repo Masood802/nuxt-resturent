679907615a432fdb9b30dc7692d80444e4d62c3e
Malik Masood <masoodsse@gmail.com>
2024-05-08 13:22:13 +0500

    fixed design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+  ],
+  css: ['nvd-u/u-core.scss'],
+  components: [
+    { path: 'components', pathPrefix: false },
+    {path:'node_modules/nvd-u/components',pathPrefix:false}
   ],
   devtools: { enabled: true }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="text-center bg-[url('bg.jpg')] bg-cover bg-center w-full h-screen bg-opacity-20 flex item-center justify-center">
-    <div class="flex items-center justify-center flex-col w-full">
-      <h1 class="text-5xl text-amber-600 font-bold py-4">Find <span class="text-red-600">best Resuturents</span> near you</h1>
-      <div class="w-[80%] my-10 ">
-            <input type="text" class="py-4 w-[30%] border-r-2 border-zinc-100 outline-none" name="search">
-            <button class="text-center bg-neutral-200 py-4 px-4 ">Detect</button>   
-            <input type="text" class="py-4 w-[30%] outline-none " name="search">
-            <label for="search" class="bg-amber-500 py-4 px-6 rounded-e">Search</label> 
-            
+  <div class="text-center bg-[url('bg.jpg')] bg-cover bg-center w-full min-h-screen bg-fixed bg-opacity-20 flex item-center justify-center">
+    <div class="flex flex-col w-full items-center h-screen">
+      <h1 class="text-5xl text-amber-600 font-bold">Find <span class="text-red-600">best Resuturents</span> near you</h1>
+      <div class="w-[80%] my-2 ">
+            <USelect 
+            class="w-[30%]"
+            :options="cities"
+            />
+            <button class="text-center bg-neutral-300 py-3 px-4 rounded-e">Detect</button>   
+             <USelect 
+            class="w-[30%]"
+            :options="search"
+            />
+            <button class="bg-amber-500 py-3 px-4 rounded-e">Search</button> 
+             
           </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import USelect from 'nvd-u/components/USelect.vue';
 
+const cities=['Lahore','karachi','Islamabad','Rawalpindi','Rahimyarkhan']
 </script>
 
-<style scoped>
+<style>
 
 </style>
