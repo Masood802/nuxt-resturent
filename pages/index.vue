355f93adca0b5ebebb27e7c1283c8e71ca43eb3e
Malik Masood <masoodsse@gmail.com>
2024-05-08 12:41:22 +0500

    added bg and other elements

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="text-3xl font-bold text-center text-red-700 my-10 ">
-    Hello
+  <div class="text-center bg-[url('bg.jpg')] bg-cover bg-center w-full h-screen bg-opacity-20 flex item-center justify-center">
+    <div class="flex items-center justify-center flex-col w-full">
+      <h1 class="text-5xl text-amber-600 font-bold py-4">Find <span class="text-red-600">best Resuturents</span> near you</h1>
+      <div class="w-[80%] my-10 ">
+            <input type="text" class="py-4 w-[30%] border-r-2 border-zinc-100 outline-none" name="search">
+            <button class="text-center bg-neutral-200 py-4 px-4 ">Detect</button>   
+            <input type="text" class="py-4 w-[30%] outline-none " name="search">
+            <label for="search" class="bg-amber-500 py-4 px-6 rounded-e">Search</label> 
+            
+          </div>
+    </div>
   </div>
 </template>
 
