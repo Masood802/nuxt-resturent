a3e634dda66e774739a9c192a750e93f43d0587e
Malik Masood <masoodsse@gmail.com>
2024-05-26 13:35:20 +0500

    changed values

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,65 @@
 <template>
-  <div class="text-center bg-[url('bg.jpg')] bg-cover bg-center w-full min-h-screen bg-fixed bg-opacity-20 flex item-center justify-center">
+  <div class="text-center bg-[url('bg.jpg')] bg-cover bg-opacity-80 bg-center w-full h-[1000px] flex item-center justify-center">
     <div class="flex flex-col w-full items-center h-screen">
-      <h1 class="text-5xl text-amber-600 font-bold">Find <span class="text-red-600">best Resuturents</span> near you</h1>
-      <div class="w-[80%] my-2 ">
-            <USelect 
-            class="w-[30%] mx-auto text-left "
-            :options="cities"
-            label="Cities"
-            />
-            <button class="text-center bg-neutral-300 py-[13px] px-4 rounded-e">Detect</button>   
-             <USelect 
-            class="w-[30%]"
-            :options="search"
-            label="Search resturents "
-            />
-            <button class="bg-amber-500 py-[13px] px-4 rounded-e">Search</button> 
-             
-          </div>
+      <h1 class="text-5xl text-amber-600 font-bold">Find <span class="text-red-600">best Places</span> near you</h1>
+      <div class="w-[80%] my-3 flex flex-col items-center justify-center">
+        <div class="w-full">
+              <label for="cities" class="text-lg  bg-neutral-300 py-2 px-5">Select City:</label>   
+              <select name="cities" id="cities" 
+              class="w-[30%] py-[12px] text-yellow-600 outline-none" 
+              v-model="store.city">
+                <option v-for="city in store.cities" 
+                :key="city.id" 
+                :value="city.coordinates" 
+                class="py-1 my-1 text-center text-lg hover:bg-neutral-300">{{ city.name }}</option>
+            </select>
+        </div>
+        <div class="w-full my-4">
+              <label for="type" class="text-lg  bg-neutral-300 py-2 px-[48px]">Search Type:</label>   
+              <select name="types" 
+              class="w-[30%] py-[12px] text-yellow-600 outline-none" 
+              v-model="store.selectedtype">                
+              <option v-for="type in store.types" :key="type" :value="type"
+              class="py-1 my-1 text-center text-lg hover:bg-neutral-300">{{ type}}</option>
+            </select>
+        </div>
+            <button class="bg-amber-500 w-[40%] h-12 rounded text-cneter text-white text-lg font-semibold " 
+            @click="store.searchRestaurants">Search</button>        
+        </div>
+        <div v-if="store.isloading" class="flex gap-2 items-center">
+              <div class="text-semibold text-center text-amber-500">Fetching data....</div>    
+              <div id="loading"></div>
     </div>
-     <NuxtLink to="/resturents" class="text-2xl text-red-500"> Page</NuxtLink>
+                <div class="w-[60%] grid content-center align-middle my-4 mx-auto" v-else>
+                  <div  class="grid grid-cols-3 gap-3 bg-white shadow-sm  bg-opacity-70 rounded px-1 my-2 py-1"
+                  v-for="result in store.results" :key="result.id">
+                        <div class="text-center text-xl font-semibold">{{result.title}}</div>   
+                        <div class="p-1 flex items-center justify-center text-lg">Ratings: {{ result.avg_rating }} 
+                          <i class="material-icons text-amber-600 text-center  text-md">star</i> </div>  
+                          <button class="bg-amber-500 w-40 h-8 rounded text-white my-1" @click="store.getdetails(result.id)">
+                          view more</button>
+                </div>
+                </div>   
   </div>
- 
+  </div>
 </template>
-
 <script setup>
-import USelect from 'nvd-u/components/USelect.vue';
-
-const cities=['Lahore','karachi','Islamabad','Rawalpindi','Rahimyarkhan']
+import { useMainStore } from '~/stores/restaurants';
+const store = useMainStore();
 </script>
-
 <style>
+#loading{
+  display: inline-block;
+  margin:10px auto;
+  width:50px;
+  min-height: 50px;
+  border: 3px solid orange;
+  border-radius: 50%;
+  border-top-color:chocolate;
+  animation: spin 1s ease-in infinite;
+}
+@keyframes spin{
+  to{ transform: rotate(360deg);}
 
+}
 </style>
--- pages/resturents.vue
+++ pages/resturents.vue
@@ -8,7 +8,7 @@
     <div class="grid grid-cols-2 gap-4 w-[50%] ">
         <div>
             <select class="w-[80%] px-2 py-3" v-model="type">
-            <option value="resturent">Resturent</option>
+            <option value="restaurant">Restaurant</option>
         </select>
         </div>
     
@@ -21,11 +21,11 @@
         </select>
     </div>
     <div>
-        {{ places }}
+       Locaion Name: {{ name }}
     </div>
     </div>
     <button class="text-sm font-bold bg-pink-500 px-4 py-2 my-4 text-white rounded" 
-    @click="findCloseBuyButtonPressed">Find Resturent</button>
+    @click="findResturents">Find Resturent</button>
     </div>
 </template>
 
@@ -36,29 +36,47 @@ const radius =ref('')
 const lat = ref(0)
 const lng=ref(0)
 const places = ref([])
-const name=ref('')
+const name = ref('')
 const coordinates = computed(() => {
     return `${lat.value},${lng.value}`
 })
 function findLocation() {
     navigator.geolocation.getCurrentPosition(
         position => {
+            
             lat.value = position.coords.latitude
             lng.value = position.coords.longitude
+            getStreetAddressFrom ()
          },
         error => {
             console.log('Error getting location')
-        })
+        })  
+      
 }
-function findCloseBuyButtonPressed() {
-    const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
-        lat.value
-      },${lng.value}&type=${type.value}&radius=${radius.value *
-        1000}&key=AIzaSyD_fFu8jlUihqfU5u3UnAfUD7dcXf-4kwI`;
+function getStreetAddressFrom() {
+    const url=`https://maps.googleapis.com/maps/api/geocode/json?latlng=
+        ${lat.value},${lng.value}&key=AIzaSyC4800J442xrkb5zUzGSEHA5GHHnMmccgc`
+    axios.get(url)
+	.then(response => {
+        if (response.data.error_massage) {
+            console.log(response.data.error_massage);
+        }
+        else
+        {
+            console.log(response.data.results)
+            name.value = response.data.results[0].formatted_address;
+        }
+	}).catch(error => {
+		console.log(error.message);
+	});
+}
+function findResturents() {
+    const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cruise&location=
+    ${lat.value},${lng.value}&type=${type.value}&radius=${radius.value *
+        1000}&key=AIzaSyDp51TWrCe9TEG_22qFY5S2ApeVLIK1jXU`;
 	axios.get(URL).then(response => {
-        places.value = response.data.results;
-        console.log(places.value)
-		addLocationsToGoogleMaps();
+        places.value = response.data.result;
+        console.log('request sent',response)
 	}).catch(error => {
 		console.log(error.message);
 	});
